71cbde22a350978e0c8369359476f40aa439e189
Nandhini <123856724+nandhu6523@users.noreply.github.com>
2025-12-23 18:47:38 +0530

    Create style.css

---
+++ code/style.css
@@ -0,0 +1,18 @@
+body {
+    font-family: Arial;
+    background-color: #eef5ff;
+    text-align: center;
+}
+
+form input, button {
+    padding: 8px;
+    margin: 5px;
+}
+
+.card {
+    background: white;
+    padding: 10px;
+    margin: 10px auto;
+    width: 300px;
+    border-radius: 5px;
+}
