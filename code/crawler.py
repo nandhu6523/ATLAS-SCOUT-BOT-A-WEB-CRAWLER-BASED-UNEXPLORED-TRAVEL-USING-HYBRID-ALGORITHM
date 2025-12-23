9c8c25fe87d1749576d6f3e3ab3cc0b4c552ad08
Nandhini <123856724+nandhu6523@users.noreply.github.com>
2025-12-23 18:46:37 +0530

    Create crawler.py

---
+++ code/crawler.py
@@ -0,0 +1,22 @@
+def crawl_data(city):
+    data = [
+        {
+            "name": "Hidden Gorge near " + city,
+            "category": "Gorge",
+            "season": "Nov - Feb",
+            "open": "07:00 - 17:00"
+        },
+        {
+            "name": "Offbeat Lake near " + city,
+            "category": "Lake",
+            "season": "Oct - Mar",
+            "open": "06:00 - 18:00"
+        },
+        {
+            "name": "Unexplored Hills near " + city,
+            "category": "Hills",
+            "season": "Sep - Feb",
+            "open": "All Day"
+        }
+    ]
+    return data
